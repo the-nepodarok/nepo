f76b1f66ecca41702939e83541fccb79c61e35ea
the-nepodarok <kosov.a@list.ru>
2024-03-24 13:52:58 +0300

    cleaning some dirty trails

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,8 +1,5 @@
 <script>
-    import Animal from "$lib/components/misc/Animal.svelte";
     import Showcase from "$lib/components/compositional/Showcase.svelte";
 </script>
 
 <Showcase />
-
-<Animal/>
